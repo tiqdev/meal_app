8dc25c4a7321010fcd6dcf72d946519cb21ea420
Tarık KAYA <tiqqq24@gmail.com>
2023-07-23 21:23:40 +0300

    metatag düzenlendi

--- app/layout.js
+++ app/layout.js
@@ -29,6 +29,38 @@ export default function RootLayout({ children }) {
     <html lang="tr">
       <meta name="theme-color" content={"#008080"} id="theme-color-meta" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <title>Kur'an-ı Kerim Türkçe Meali</title>
+      <meta name="title" content="Kur'an-ı Kerim Türkçe Meali" />
+      <meta
+        name="description"
+        content="Bu site (Kur'an-ı Kerim Türkçe Meali), insanlara faydalı olmak ve Allah'ın ilmini insanlara yaymak amacıyla çıkar gözetmeksizin Tarık Kaya tarafından oluşturulmuştur."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://kuranmeali.vercel.app/" />
+      <meta property="og:title" content="Kur'an-ı Kerim Türkçe Meali" />
+      <meta
+        property="og:description"
+        content="Bu site (Kur'an-ı Kerim Türkçe Meali), insanlara faydalı olmak ve Allah'ın ilmini insanlara yaymak amacıyla çıkar gözetmeksizin Tarık Kaya tarafından oluşturulmuştur."
+      />
+      <meta
+        property="og:image"
+        content="https://kuranmeali.vercel.app/og-image.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://kuranmeali.vercel.app/" />
+      <meta property="twitter:title" content="Kur'an-ı Kerim Türkçe Meali" />
+      <meta
+        property="twitter:description"
+        content="Bu site (Kur'an-ı Kerim Türkçe Meali), insanlara faydalı olmak ve Allah'ın ilmini insanlara yaymak amacıyla çıkar gözetmeksizin Tarık Kaya tarafından oluşturulmuştur."
+      />
+      <meta
+        property="twitter:image"
+        content="https://kuranmeali.vercel.app/og-image.png"
+      />
+
       <meta name="robots" content="index, follow" />
       <meta name="googlebot" content="index, follow" />
       <link rel="manifest" href="/manifest.json" />
