c4a5f9e9145e9fc61b35f0bc58ea409b897d9c78
Tarık KAYA <tiqqq24@gmail.com>
2023-09-06 09:37:47 +0300

    kendim için attıgım build geri alındı

--- components/SettingsList.jsx
+++ components/SettingsList.jsx
@@ -1,17 +1,6 @@
-"use client";
-
 import Link from "next/link";
-import { exportFavList, getFavList } from "@/utils/localStorageManager";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
 
 const SettingsList = () => {
-  const [favList, setFavList] = useState([]);
-
-  useEffect(() => {
-    setFavList(getFavList());
-  }, []);
-
   return (
     <>
       <Link href={`/authors`} className="surah_item ">
@@ -23,20 +12,6 @@ const SettingsList = () => {
       <Link href={`/about`} className="surah_item">
         <p className="font-[500] text-[1.3rem]">Uygulama Hakkında</p>
       </Link>
-      {favList.length > 0 && (
-        <div
-          className="surah_item"
-          onClick={() => {
-            try {
-              exportFavList();
-            } catch (e) {
-              toast("İndirme işlemi başarısız.");
-            }
-          }}
-        >
-          <p className="font-[500] text-[1.3rem]">Kayıtlı Ayetleri İndir</p>
-        </div>
-      )}
     </>
   );
 };
